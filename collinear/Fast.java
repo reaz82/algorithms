5d5adb3611109099ef10eac0fd4d57de9c5242f0
Reaz Hasan <rhasan@gmail.com>
2015-02-18 23:47:08 -0600

    Each point is compared against the origin and only added to a segment accordingly. This eliminates permutations and subsegments.

--- collinear/Fast.java
+++ collinear/Fast.java
@@ -9,13 +9,13 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Fast {
-  private static void print( LinkedList<Point> segment ) {
+  private static void print(LinkedList<Point> segment) {
     Point first = segment.removeFirst();
     Point next = first;
     StdOut.print(first);
     while (segment.size() > 0) {
       StdOut.print(" -> ");
-      next = segment.removeFirst();
+      next = segment.removeLast();
       StdOut.print(next);
     }
     StdOut.println();
@@ -42,27 +42,33 @@ public class Fast {
 
     double slope = 0.0;
     double prevSlope = 0.0;
-
+    Arrays.sort(points, 0, numberOfPoints);
     LinkedList<Point> segment, oldsegment;
     for (int i = 0; i < numberOfPoints; i++) {
 
       segment = new LinkedList<Point>();
-
-      Arrays.sort(temp, 0, (numberOfPoints-1), points[i].SLOPE_ORDER);
+      //StdOut.print("Base Point: ");
+      //StdOut.println(points[i]);
+      Arrays.sort(temp, 0, numberOfPoints, points[i].SLOPE_ORDER);
       segment.addLast(points[i]);
 
       for (int j = 0; j < numberOfPoints; j++) { // ignore the root point p.
         slope = points[i].slopeTo(temp[j]);
-
+        //StdOut.print("Compare point: ");
+        //StdOut.print(temp[j]);
+        //StdOut.print(" : ");
+        //StdOut.println(slope);
         if (slope == Double.NEGATIVE_INFINITY) continue; // ignore degenerate point
         if (slope == prevSlope) { // keep track of this point. Never true for first non-first degenerate point.
-          segment.addLast(temp[j]);
+          if (temp[0].compareTo(temp[j]) < 0) {
+            segment.addLast(temp[j]);
+          } else break;
         } else { // if the slope is not equal to previous then we no longer have a connecting points
                  // if the segment has more then 3 points then we have a line segment or else we discard.
           if (segment.size() > 3) {
             print(segment);
           }
-          break;
+          continue;
         }
         prevSlope = slope;
       }
