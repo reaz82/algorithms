35e3ae1f70ff74f14cdbf8b78e1ce649220c3179
Reaz Hasan <rhasan@gmail.com>
2015-08-23 08:46:04 -0500

    updated

---
+++ 8puzzle/src/Solver.java
@@ -0,0 +1,10 @@
+/**
+ * Created by rhasan on 2/21/15.
+ */
+public class Solver {
+    public Solver(Board initial) {}
+    public boolean isSolvable() {}
+    public int moves() {}
+    public Iterable<Board> solution() {}
+    public static void main(String[] args) {}
+}
